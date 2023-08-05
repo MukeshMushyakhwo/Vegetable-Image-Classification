a3b46ba4447cef5be76fa639fb9abf758caeb7ab
MukeshMushyakhwo <mukeshmushyakhwo01@gmail.com>
2023-08-05 14:04:22 +0545

    Decrease image display size

--- classification.py
+++ classification.py
@@ -29,17 +29,24 @@ def predict(image):
     img = np.expand_dims(img, axis=0)
     prediction = model.predict(img)
     predicted_class = np.argmax(prediction)
+
+    # Display output size of 120 x 120
+    st.image(img, caption='Uploaded Image.', width=400)
+    
+     # Calculate prediction accuracy
+    prediction_accuracy = np.max(prediction)
+
+    # Display prediction accuracy
+    st.write(f"Prediction accuracy: {prediction_accuracy:.2f}")
+
     return predicted_class
 
 def main():
     st.title("Vegetable Classifier")
 
-    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    uploaded_image = st.file_uploader("Upload an image  [Bean, Bitter Gourd, Bottle Gourd, Brinjal,Broccoli, Cabbage, Capsicum, Carrot, Cauliflower, Cucumber, Papaya, Potato, Pumpkin, Radish, Tomato]", type=["jpg", "jpeg", "png"])
 
     if uploaded_image is not None:
-        # Display uploaded image
-        st.image(uploaded_image, caption='Uploaded Image.', use_column_width=True)
-
         # Make prediction
         predicted_class = predict(uploaded_image)
         st.write(f"Prediction: {labels[predicted_class]}")
